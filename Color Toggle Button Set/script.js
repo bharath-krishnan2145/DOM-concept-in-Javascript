d3405386f850f526a59ca1ba104135a98323d100
Bharath Krishnan J <bharathkrishnan.jbk@gmail.com>
2024-10-04 14:37:42 +0530

    Add files via upload

---
+++ Color Toggle Button Set/script.js
@@ -0,0 +1,22 @@
+var body=document.body;
+
+
+function change(){
+    body.style.backgroundColor="red";
+    //box.style.backgroundColor=""
+   
+}
+
+function change1(){
+    body.style.backgroundColor="blue";
+    //box1.style.backgroundColor=""
+}
+
+function change2(){
+    body.style.backgroundColor="green";
+    //box2.style.backgroundColor=""
+}
+function change3(){
+     body.style.backgroundColor="white";
+    //box2.style.backgroundColor=""
+}
